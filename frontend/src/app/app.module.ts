d06f81f3b5dbead0f1d7e0bd61399696d48d25f7
SPENCER.EXE <khushalrdhumal@gmail.com>
2024-04-22 12:32:28 +0530

    Added Cart add-remove functionality

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { SearchComponent } from './components/partials/search/search.component';
 import { FoodPageComponent } from './components/pages/food-page/food-page.component';
 import { TagsComponent } from './components/partials/tags/tags.component';
 import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { TitleComponent } from './components/partials/title/title.component';
 
 
 @NgModule({
@@ -20,6 +21,7 @@ import { CartPageComponent } from './components/pages/cart-page/cart-page.compon
         FoodPageComponent,
         TagsComponent,
         CartPageComponent,
+        TitleComponent,
         
         // HeaderComponent
     ],
