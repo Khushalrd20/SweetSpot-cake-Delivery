f208730fdc090779bf461a1e6b178864f9887216
SPENCER.EXE <khushalrdhumal@gmail.com>
2024-05-07 10:27:07 +0530

    Added Login page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { TagsComponent } from './components/partials/tags/tags.component';
 import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
 import { TitleComponent } from './components/partials/title/title.component';
 import { NotFoundComponent } from './components/partials/not-found/not-found.component';
+import { LoginPageComponent } from './components/pages/login-page/login-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -24,6 +26,8 @@ import { NotFoundComponent } from './components/partials/not-found/not-found.com
         CartPageComponent,
         TitleComponent,
         NotFoundComponent,
+        LoginPageComponent,
+       
         
         // HeaderComponent
     ],
@@ -35,7 +39,8 @@ import { NotFoundComponent } from './components/partials/not-found/not-found.com
         BrowserModule,
         AppRoutingModule,
         HeaderComponent,
-        HttpClientModule
+        HttpClientModule,
+        ReactiveFormsModule
         
         
     ]
